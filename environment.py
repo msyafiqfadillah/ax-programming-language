14f686125f573be0ae80ad85c1a55afd300542d6
Muhammad Syafiq Fadillah <codingan77@gmail.com>
2026-01-04 21:29:06 +0700

    add environment

---
+++ environment.py
@@ -0,0 +1,26 @@
+class Environment:
+    def __init__(self, record = {}, parent = None):
+        self.record = record
+        self.parent = parent
+
+    def define(self, name, value):
+        self.record[name] = value
+
+        return value
+
+    def assign(self, name, value):
+        self.resolve(name).record = value
+
+        return value
+    
+    def lookup(self, name):
+        return self.resolve(name).record[name]
+
+    def resolve(self, name):
+        if (name in self.record.keys()):
+            return self
+        
+        if (self.parent is None):
+            raise ReferenceError(f"Variable {name} is not defined")
+        
+        return self.parent.resolve(name)
