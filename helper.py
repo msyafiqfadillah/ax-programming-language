0fb552b5ea83a37c5d1027ddf96201a61941969e
Muhammad Syafiq Fadillah <codingan77@gmail.com>
2026-01-18 14:11:55 +0700

    minor update

--- helper.py
+++ helper.py
@@ -1,2 +1,11 @@
+from tokens.keywords import Keywords
+
+
 def is_eof(c_index, data):
     return c_index >= len(data)
+
+def bool_converter(value):
+    return Keywords.TRUE if value else Keywords.FALSE
+
+def is_truhty(value):
+    return value not in ("", Keywords.FALSE, Keywords.EMPTY, Keywords.UNDEFINED)
