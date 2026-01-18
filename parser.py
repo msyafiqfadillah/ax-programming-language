8ebebb2e1353a6945a9233085c642c03be6b0402
Muhammad Syafiq Fadillah <codingan77@gmail.com>
2026-01-18 14:12:47 +0700

    applied new grammar

--- parser.py
+++ parser.py
@@ -1,95 +1,9 @@
 import helper
+import nodes
 from tokens.keywords import Keywords
-from tokens.punctions import Punctions
+from tokens.punctuations import Punctuations
 from tokens.operators import Operators
 
-
-class Program:
-    def __init__(self, body):
-        self.body = body
-
-class VariableDeclaration:
-    def __init__(self, kind, declaration):
-        self.kind = kind
-        self.declaration = declaration
-
-    def __repr__(self):
-        return f"{self.kind} {self.declaration.id.name} = {self.declaration.init}"
-
-class VariableDeclarator:
-    def __init__(self, id, init):
-        self.id = id
-        self.init = init
-
-    def __repr__(self):
-        return f"{self.id.name} = {self.init}"
-
-class BinaryExpression:
-    def __init__(self, left, operator, right):
-        self.left = left
-        self.operator = operator
-        self.right = right
-
-    def __repr__(self):
-        return f"{self.left} {self.operator} {self.right}"
-
-class Identifier:
-    def __init__(self, name):
-        self.name = name
-
-    def __repr__(self):
-        return self.name
-
-class Literal:
-    def __init__(self, value):
-        self.value = value
-
-    def __repr__(self):
-        return str(self.value)
-    
-class GroupedExpression:
-    def __init__(self, opening, expr, closing):
-        self.opening = opening
-        self.expr = expr
-        self.closing = closing
-
-    def __repr__(self):
-        return f"{self.opening} {self.expr} {self.closing}"
-    
-class FunctionDeclaration:
-    def __init__(self, name, params, body):
-        self.name = name
-        self.params = params
-        self.body = body
-
-    def __repr__(self):
-        params = ", ".join(repr(s) for s in self.params)
-
-        return f"prc {self.name}({params}) {self.body}"
-    
-class CallExpression:
-    def __init__(self, callee, arguments):
-        self.callee = callee
-        self.arguments = arguments
-
-    def __repr__(self):
-        args = ", ".join(repr(s) for s in self.arguments)
-        
-        return f"{self.callee}({args})"
-    
-class BlockStatement:
-    def __init__(self, body):
-        self.body = body
-
-    def __repr__(self):
-        return "{ " + " ".join(repr(s) for s in self.body) + " }"
-    
-class ReturnStatement:
-    def __init__(self, argument):
-        self.argument = argument
-
-    def __repr__(self):
-        return f"return {self.argument}"
 
 class Parser:
     def __init__(self):
@@ -118,7 +32,7 @@ class Parser:
 
             self.body.append(stmt)
 
-        return Program(body=self.body)
+        return nodes.Program(body=self.body)
 
     def parse_statement(self):
         current_token = self.peek()
@@ -132,6 +46,7 @@ class Parser:
         elif (current_token["value"] == Keywords.RETURN):
             return self.parse_return()
         elif (current_token["type"] == "IDENTIFIER" or current_token["type"] == "NUMBER"):
+        # else:
             return self.parse_expression()
 
         raise TypeError(f"Unknown statement starting with {self.peek()["value"]}")
@@ -142,7 +57,7 @@ class Parser:
         self.match(Operators.EQUAL, "value")
         expression = self.parse_expression()
 
-        return VariableDeclaration(kind="var", declaration=VariableDeclarator(id=Identifier(name=identifier["value"]), init=expression))
+        return nodes.VariableDeclaration(kind="var", declaration=nodes.VariableDeclarator(id=nodes.Identifier(name=identifier["value"]), init=expression))
     
     def parse_set(self):
         self.match(Keywords.SET, "value")
@@ -150,72 +65,29 @@ class Parser:
         self.match(Operators.EQUAL, "value")
         expression = self.parse_expression()
 
-        return VariableDeclaration(kind="set", declaration=VariableDeclarator(id=Identifier(name=identifier["value"]), init=expression))
-
-    def parse_expression(self):
-        left = self.parse_term()
-
-        while (not helper.is_eof(self.index, self.tokens) and self.peek()["type"] == "OPERATOR"):
-            operator = self.match("OPERATOR", "type")["value"]
-            right = self.parse_term()
-
-            left = BinaryExpression(left=left, operator=operator, right=right)
-
-        return left
- 
-    def parse_term(self):
-        current = self.peek()
-        
-        if (current["type"] == "NUMBER"):
-            self.match("NUMBER", "type")
-
-            return Literal(current["value"])
-        elif (current["type"] == "IDENTIFIER"):
-            self.match("IDENTIFIER", "type")
-            next_peek = self.peek()
-
-            if (next_peek["value"] == "("):
-                self.match(Punctions.PARANTHESSES_O, "value")
-                args = self.parse_args()
-                self.match(Punctions.PARANTHESSES_C, "value")
-
-                return CallExpression(Identifier(current["value"]), args)
-            else:
-                return Identifier(current["value"])
-        elif (current["value"] == "("):
-            self.match(Punctions.PARANTHESSES_O, "value")
-            expr = self.parse_expression()
-            self.match(Punctions.PARANTHESSES_C, "value")
-
-            return GroupedExpression(Punctions.PARANTHESSES_O, expr, Punctions.PARANTHESSES_C)
-        elif (current["type"] == "STRING"):
-            self.match("STRING", "type")
-
-            return Literal(current["value"])
-        
-        raise TypeError(f"Expected term, but got {current["value"]}")
+        return nodes.VariableDeclaration(kind="set", declaration=nodes.VariableDeclarator(id=nodes.Identifier(name=identifier["value"]), init=expression))
     
     def parse_function(self):
         self.match(Keywords.PRC, "value")
         identifier = self.match("IDENTIFIER", "type")
-        self.match(Punctions.PARANTHESSES_O, "value")
+        self.match(Punctuations.PARANTHESSES_O, "value")
         params = self.parse_params()
-        self.match(Punctions.PARANTHESSES_C, "value")
-        self.match(Punctions.CURVED_O, "value")
+        self.match(Punctuations.PARANTHESSES_C, "value")
+        self.match(Punctuations.CURVED_O, "value")
         body = self.parse_block()
-        self.match(Punctions.CURVED_C, "value")
+        self.match(Punctuations.CURVED_C, "value")
 
-        return FunctionDeclaration(Identifier(identifier["value"]), params, body)
+        return nodes.FunctionDeclaration(nodes.Identifier(identifier["value"]), params, body)
     
     def parse_block(self):
         body = []
 
-        while (self.peek()["value"] != Punctions.CURVED_C):
+        while (self.peek()["value"] != Punctuations.CURVED_C):
             statement = self.parse_statement()
 
             body.append(statement)
 
-        return BlockStatement(body)
+        return nodes.BlockStatement(body)
     
     def parse_params(self):
         params = []
@@ -223,14 +95,14 @@ class Parser:
         while (self.peek()["type"] == "IDENTIFIER"):
             param = self.match("IDENTIFIER", "type")["value"]
 
-            params.append(Identifier(param))
+            params.append(nodes.Identifier(param))
 
         return params
     
     def parse_args(self):
         params = []
 
-        while (not helper.is_eof(self.index, self.tokens) and self.peek()["value"] != Punctions.PARANTHESSES_C):
+        while (not helper.is_eof(self.index, self.tokens) and self.peek()["value"] != Punctuations.PARANTHESSES_C):
             param = self.parse_expression()
 
             params.append(param)
@@ -241,8 +113,208 @@ class Parser:
         self.match(Keywords.RETURN, "value")
         expr = self.parse_expression()
 
-        return ReturnStatement(expr)
+        return nodes.ReturnStatement(expr)
 
+    def parse_expression(self):
+        return self.parse_logical_or()
+
+    def parse_logical_or(self):
+        node_logical_and = self.parse_logical_and()
+
+        while (not helper.is_eof(self.index, self.tokens) and self.peek()["value"] == Operators.OR):
+            current = self.match(self.peek()["value"], "value")
+            right = self.parse_logical_and()
+            
+            node_logical_and = nodes.BinaryExpression(node_logical_and, current["value"], right)
+
+        return node_logical_and
+
+    def parse_logical_and(self):
+        node_equality = self.parse_equality()
+
+        while (not helper.is_eof(self.index, self.tokens) and self.peek()["value"] == Operators.AND):
+            current = self.match(self.peek()["value"], "value")
+            right = self.parse_equality()
+            
+            node_equality = nodes.BinaryExpression(node_equality, current["value"], right)
+
+        return node_equality
+
+    def parse_equality(self):
+        node_comparison = self.parse_comparison()
+
+        while (not helper.is_eof(self.index, self.tokens) 
+               and self.peek()["value"] in (
+                   Operators.D_EQUAL, 
+                   Operators.N_EQUAL
+                )):
+            current = self.match(self.peek()["value"], "value")
+            right = self.parse_comparison()
+            
+            node_comparison = nodes.BinaryExpression(node_comparison, current["value"], right)
+
+        return node_comparison
+
+    def parse_comparison(self):
+        node_additive = self.parse_additive()
+
+        while (not helper.is_eof(self.index, self.tokens) 
+               and self.peek()["value"] in (
+                   Operators.GREATER, 
+                   Operators.LESS, 
+                   Operators.G_EQUAL, 
+                   Operators.L_EQUAL
+                )):
+            current = self.match(self.peek()["value"], "value")
+            right = self.parse_additive()
+            
+            node_additive = nodes.BinaryExpression(node_additive, current["value"], right)
+
+        return node_additive
+
+    def parse_additive(self):
+        node_multiplicative = self.parse_multiplicative()
+
+        while (not helper.is_eof(self.index, self.tokens) 
+                and self.peek()["value"] in (
+                    Operators.SUBTRACTION, 
+                    Operators.ADDITION
+                )):
+            current = self.match(self.peek()["value"], "value")
+            right = self.parse_multiplicative()
+            
+            node_multiplicative = nodes.BinaryExpression(node_multiplicative, current["value"], right)
+
+        return node_multiplicative
+
+    def parse_multiplicative(self):
+        node_power = self.parse_power()
+
+        while (not helper.is_eof(self.index, self.tokens) 
+               and self.peek()["value"] in (
+                   Operators.MULTIPLICATION, 
+                   Operators.DIVISION, 
+                   Operators.MODULO
+                )):
+            current = self.match(self.peek()["value"], "value")
+            right = self.parse_power()
+            
+            node_power = nodes.BinaryExpression(node_power, current["value"], right)
+
+        return node_power
+
+    def parse_power(self):
+        node_unary = self.parse_unary()
+
+        if (not helper.is_eof(self.index, self.tokens) and self.peek()["value"] == Operators.POWER):
+            operator = self.match(Operators.POWER, "value")
+            right = self.parse_power()
+
+            return nodes.BinaryExpression(node_unary, operator["value"], right)
+
+        return node_unary
+    
+    def parse_unary(self):
+        # TODO : Implement Unary Operator
+        return self.parse_primary()
+    
+    def parse_primary(self):
+        node_atom = self.parse_atom()
+
+        if (not helper.is_eof(self.index, self.tokens) and self.peek()["value"] == Punctuations.PARANTHESSES_O):
+            self.match(Punctuations.PARANTHESSES_O, "value")
+            args = self.parse_args()
+            self.match(Punctuations.PARANTHESSES_C, "value")
+
+            return nodes.CallExpression(node_atom, args)
+        
+        return node_atom
+    
+    def parse_atom(self):
+        current = self.peek()
+        
+        if (current["type"] in ("STRING", "NUMBER") 
+            or (current["type"] == "KEYWORDS" 
+                and current["value"] in ("true", "false", "empty", "undefined"))):
+            return self.parse_literal()
+        elif (current["type"] == "IDENTIFIER"):
+            self.match("IDENTIFIER", "type")
+
+            return nodes.Identifier(current["value"])
+        elif (current["value"] == "("):
+            self.match(Punctuations.PARANTHESSES_O, "value")
+            expr = self.parse_args()
+            self.match(Punctuations.PARANTHESSES_C, "value")
+
+            return nodes.GroupedExpression(Punctuations.PARANTHESSES_O, expr, Punctuations.PARANTHESSES_C)
+        elif (current["value"] == Punctuations.V_LINE):
+            return self.parse_list()
+        elif (current["value"] == Punctuations.SQUARE_O):
+            return self.parse_hashmap()
+
+        # raise TypeError(f"Expected atom, but got {current["value"]}")
+
+    def parse_list(self):
+        self.match(Punctuations.V_LINE, "value")
+        expr = self.parse_args()
+        self.match(Punctuations.V_LINE, "value")
+
+        return nodes.ListExpression(expr)
+
+    def parse_hashmap(self):
+        self.match(Punctuations.CURVED_O, "value")
+        
+        items = { }
+
+        while (not helper.is_eof(self.index, self.tokens) and self.peek()["value"] != Punctuations.CURVED_C):
+            key = self.match("STRING", "type")
+            self.match(":", "value")
+            value = self.parse_expression()
+
+            items[key] = value
+
+        self.match(Punctuations.CURVED_C, "value")
+
+        return nodes.HashmapExpression(items)
+    
+    def parse_literal(self):
+        current = self.peek()
+
+        if (current["type"] == "STRING"):
+            return self.parse_string()
+        elif (current["type"] == "NUMBER"):
+            return self.parse_number()
+        elif (current["type"] == "KEYWORDS" and current["value"] in ("true", "false")):
+            return self.parse_boolean()
+        elif (current["type"] == "KEYWORDS" and current["value"] == "empty"):
+            return self.parse_empty()
+        elif (current["type"] == "KEYWORDS" and current["value"] == "undefined"):
+            return self.parse_undefined()
+
+    def parse_string(self):
+        value = self.match("STRING", "type")["value"]
+
+        return nodes.Literal(value)
+
+    def parse_number(self):
+        value = self.match("NUMBER", "type")["value"]
+
+        return nodes.Literal(value)
+
+    def parse_boolean(self):
+        value = self.match(self.peek()["value"], "value")["value"]
+
+        return nodes.Literal(value)
+
+    def parse_empty(self):
+        self.match(Keywords.EMPTY, "value")
+
+        return nodes.Literal(Keywords.EMPTY)
+    
+    def parse_undefined(self):
+        self.match(Keywords.UNDEFINED, "value")
+
+        return nodes.Literal(Keywords.UNDEFINED)
 
 def main():
     parser = Parser()
@@ -251,68 +323,9 @@ def main():
         {"type": "KEYWORDS", "value": "var"},
         {"type": "IDENTIFIER", "value": "x"},
         {"type": "OPERATOR", "value": "="},
-        {"type": "NUMBER", "value": "42"},
-        {"type": "KEYWORDS", "value": "var"},
-        {"type": "IDENTIFIER", "value": "y"},
-        {"type": "OPERATOR", "value": "="},
-        {"type": "NUMBER", "value": "99"},
-        {"type": "KEYWORDS", "value": "var"},
-        {"type": "IDENTIFIER", "value": "z"},
-        {"type": "OPERATOR", "value": "="},
-        {"type": "IDENTIFIER", "value": "x"},
-        {"type": "OPERATOR", "value": "+"},
-        {"type": "PUNCTION", "value": "("},
-        {"type": "IDENTIFIER", "value": "y"},
-        {"type": "OPERATOR", "value": "+"},
-        {"type": "NUMBER", "value": "3"},
-        {"type": "PUNCTION", "value": ")"},
-        {"type": "KEYWORDS", "value": "var"},
-        {"type": "IDENTIFIER", "value": "_g"},
-        {"type": "OPERATOR", "value": "="},
-        {"type": "STRING", "value": "ABC"},
-        {"type": "KEYWORDS", "value": "set"},
-        {"type": "IDENTIFIER", "value": "x"},
-        {"type": "OPERATOR", "value": "="},
-        {"type": "NUMBER", "value": "32"},
-        {"type": "OPERATOR", "value": "%"},
+        {"type": "NUMBER", "value": "6"},
+        {"type": "OPERATOR", "value": "<"},
         {"type": "NUMBER", "value": "2"},
-        {"type": "KEYWORDS", "value": "set"},
-        {"type": "IDENTIFIER", "value": "x"},
-        {"type": "OPERATOR", "value": "="},
-        {"type": "IDENTIFIER", "value": "y"},
-        {"type": "OPERATOR", "value": "*"},
-        {"type": "IDENTIFIER", "value": "z"},
-        {"type": "OPERATOR", "value": "+"},
-        {"type": "NUMBER", "value": "22"},
-        {"type": "KEYWORDS", "value": "prc"},
-        {"type": "IDENTIFIER", "value": "foo"},
-        {"type": "PUNCTION", "value": "("},
-        {"type": "IDENTIFIER", "value": "param_1"},
-        {"type": "IDENTIFIER", "value": "param_2"},
-        {"type": "PUNCTION", "value": ")"},
-        {"type": "PUNCTION", "value": "{"},
-        {"type": "KEYWORDS", "value": "var"},
-        {"type": "IDENTIFIER", "value": "new_x"},
-        {"type": "OPERATOR", "value": "="},
-        {"type": "NUMBER", "value": "12"},
-        {"type": "KEYWORDS", "value": "set"},
-        {"type": "IDENTIFIER", "value": "new_x"},
-        {"type": "OPERATOR", "value": "="},
-        {"type": "NUMBER", "value": "99"},
-        {"type": "KEYWORDS", "value": "set"},
-        {"type": "IDENTIFIER", "value": "new_x"},
-        {"type": "OPERATOR", "value": "="},
-        {"type": "PUNCTION", "value": "("},
-        {"type": "IDENTIFIER", "value": "new_x"},
-        {"type": "OPERATOR", "value": "+"},
-        {"type": "NUMBER", "value": "2"},
-        {"type": "PUNCTION", "value": ")"},
-        {"type": "OPERATOR", "value": "*"},
-        {"type": "NUMBER", "value": "99"},
-        {"type": "PUNCTION", "value": "}"},
-        {"type": "IDENTIFIER", "value": "foo"},
-        {"type": "PUNCTION", "value": "("},
-        {"type": "PUNCTION", "value": ")"},
     ]
 
     for token in parser.parse_program(sample).body:
